4928886b24a8eff038f765458c0e6055492a4edb
Emily Phee <ephee@uci.edu>
2022-08-11 14:44:15 -0700

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,28 @@
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=' +
+        apiKey,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
